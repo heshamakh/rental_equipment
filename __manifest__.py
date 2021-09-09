c123057f1b7537f356717eb086db8a50ab918f60
Hesham Abou Khair <hesham.a.kh@gmail.com>
2021-09-09 22:32:43 +0300

    RE4 FIX add view

--- __manifest__.py
+++ __manifest__.py
@@ -4,6 +4,7 @@
     'depends': ['base'],
     'data': [
         'security/ir.model.access.csv'
+        'views/rental_equipment_category_view.xml'
     ],
 	'installable': True,
     'application': True
