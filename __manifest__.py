5cec89ed21a676d634720c94a51f45faf4aa6994
Hesham Abou Khair <hesham.a.kh@gmail.com>
2021-09-09 20:41:14 +0300

    RE2 ADD main files

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Rental Equipment',
+    'summary': 'track equipment rental process',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv'
+    ],
+	'installable': True,
+    'application': True
+}
