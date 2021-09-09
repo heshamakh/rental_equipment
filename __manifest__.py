618ffce397609fb4a5d551058089fe88b755d718
Hesham Abou Khair <hesham.a.kh@gmail.com>
2021-09-09 22:47:07 +0300

    RE4 ADD action view

--- __manifest__.py
+++ __manifest__.py
@@ -3,8 +3,9 @@
     'summary': 'track equipment rental process',
     'depends': ['base'],
     'data': [
-        'security/ir.model.access.csv'
-        'views/rental_equipment_category_view.xml'
+        'security/ir.model.access.csv',
+        'views/rental_equipment_category_view.xml',
+        'views/rental_menus.xml'
     ],
 	'installable': True,
     'application': True
